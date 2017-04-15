362ae1dacd950138418a04c2abeb85123b91438e
Dens98 <9e:0b:4a:12:7e:2c:76:25:92:67:eb:13:92:64:42:b3>
2017-04-15 15:42:19 +0300

    App desing

--- VKBunch/ViewController.swift
+++ VKBunch/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import VK_ios_sdk
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myPhotoSetting: UIImageView?
     override func viewDidAppear(_ animated: Bool) {
         
             }
@@ -18,23 +19,19 @@ class ViewController: UIViewController {
         VKHelper.shared.logout()
         print("logOut")
     }
-    
+    let qwer: Bool = true
     @IBAction func ButtonLogIn(_ sender: UIButton) {
 
-        
-        VKHelper.shared.getFriends(in: self, count: 100, offset: 0) { (userArray, error) in
+       VKHelper.shared.getFriends(in: self, count: 100, offset: 0) { (userArray, error) in
             
             
             print("\(String(describing: userArray?[0].first_name))", "\(String(describing: userArray?[0].id))")
             }
-        
 //        VKHelper.shared.getFriends(in: self) { (error) in
 //            
 //            print(error)
 //        }
 
-        
-        //print("Hi")
     }
     
 }
