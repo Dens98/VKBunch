bf3f4c0550b5509065af407e626c8275c9349441
aryabtsev <artem512@inbox.ru>
2017-05-05 17:12:04 +0300

    friends

--- VKBunch/Friends.swift
+++ VKBunch/Friends.swift
@@ -9,10 +9,21 @@
 import Foundation
 
 
-class Friends{
-    var user_id = ""
+class Friends: SearchViewController{
+    var user_id = 0
     var name = ""
     var surname = ""
     var photo = ""
+    
+    
+    func addingFriends(identificator: Any){
+        self.user_id = identificator as! Int
+        }
+    
+    func addingAdditionalInfo(name: String, surname: String, photo: String){
+        self.name = name
+        self.surname = surname
+        self.photo = photo
+    }
     
 }
